2dd2806d8b09803b4a7450928937c780fa6bd0ae
Ilya Ignatenko <ilya.ilya.ignatenko@gmail.com>
2023-08-24 18:52:10 +1000

    Homework Done!

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name varchar(15) NOT NULL,
+	last_name varchar(15) NOT NULL,
+	title varchar(60) NOT NULL,
+	birth_date date NOT NULL,
+	notes text
+);
+
+
+CREATE TABLE customers
+(
+	customer_id varchar(20) PRIMARY KEY,
+	company_name varchar(40) NOT NULL, 
+	contact_name varchar(50) NOT NULL
+);
+
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id varchar(20) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+    ship_city varchar(40) NOT NULL
+);
+
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,29 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+conn = psycopg2.connect("host=localhost dbname=north user=postgres password=12345")
+cur = conn.cursor()
+
+with open('north_data/customers_data.csv', 'r', encoding="utf-8") as f:
+    next(f)
+    cur.copy_from(f, 'customers', sep=',')
+
+with open('north_data/employees_data.csv', 'r', encoding='utf-8') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader)
+    for row in csv_reader:
+        employee_id, first_name, last_name, title, birth_date, notes = row
+
+        insert_query = f"INSERT INTO {'employees'} (employee_id, first_name, last_name, title, birth_date, notes) VALUES (%s, %s, %s, %s, %s, %s)"
+        cur.execute(insert_query, (employee_id, first_name, last_name, title, birth_date, notes))
+
+with open('north_data/orders_data.csv', 'r', encoding="utf-8") as f:
+    next(f)
+    cur.copy_from(f, 'orders', sep=',')
+
+conn.commit()
+
+# close cursor and connection
+cur.close()
+conn.close()
